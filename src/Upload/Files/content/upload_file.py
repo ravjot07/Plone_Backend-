b8313ef7ef0188d76fecf64ea3984ef76393a0d4
ravjot07 <ravu2004@gmail.com>
2024-04-02 06:02:27 +0530

    plone backend

--- src/Upload/Files/content/upload_file.py
+++ src/Upload/Files/content/upload_file.py
@@ -10,14 +10,22 @@ from plone.supermodel import model
 # from z3c.form.browser.radio import RadioFieldWidget
 # from zope import schema
 from zope.interface import implementer
-
+from plone.namedfile.field import NamedBlobFile
 
 # from Upload.Files import _
 
 
 class IUploadFile(model.Schema):
+       
+
+       
     """ Marker interface and Dexterity Python Schema for UploadFile
     """
+
+    my_file = NamedBlobFile(
+            title=u"File",
+        description=u"Upload a file."
+        )
     # If you want, you can load a xml model created TTW here
     # and customize it in Python:
 
